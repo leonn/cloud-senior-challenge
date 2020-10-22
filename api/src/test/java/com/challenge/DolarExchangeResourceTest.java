707d76c9a36e65d51cd88644215ae99fd60211f8
Leonn Paiva <leonn.paiva@gmail.com>
2020-10-22 20:30:03 -0300

    :bulb: init quarkus quickstart application

---
+++ api/src/test/java/com/challenge/DolarExchangeResourceTest.java
@@ -0,0 +1,21 @@
+package com.challenge;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class DolarExchangeResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
---
+++ api/src/test/java/com/challenge/NativeDolarExchangeResourceIT.java
@@ -0,0 +1,9 @@
+package com.challenge;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeDolarExchangeResourceIT extends DolarExchangeResourceTest {
+
+    // Execute the same tests but in native mode.
+}
