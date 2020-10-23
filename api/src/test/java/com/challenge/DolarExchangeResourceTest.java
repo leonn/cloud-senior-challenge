d7c8cac4c5ab1bf3557ba3b86bf04931603a7988
Leonn Paiva <leonn.paiva@gmail.com>
2020-10-22 21:06:11 -0300

    :repeat: setup docker & docker-compose

--- api/src/test/java/com/challenge/DolarExchangeResourceTest.java
+++ api/src/test/java/com/challenge/DolarExchangeResourceTest.java
@@ -12,7 +12,7 @@ public class DolarExchangeResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/")
+          .when().get("/dolarExchange")
           .then()
              .statusCode(200)
              .body(is("hello"));
